2fbf0107b9483b3bfefb5a42aba9e498e54afd95
Fernanda Miyuki <miyukifernanda26@gmail.com>
2026-06-16 18:12:37 -0300

    alteracoes1606

--- frontend/src/routes/sobre.tsx
+++ frontend/src/routes/sobre.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import heroImg from "@/assets/hero-padaria.jpg";
+import heroImg from "@/assets/hero-padaria.png";
 
 export const Route = createFileRoute("/sobre")({
   head: () => ({
